b00bb5a5f0b515a5839160edfd80afe20043474c
ImVladislav <nafanya102@gmail.com>
2022-09-27 18:09:44 +0300

    finish
    finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+    bodyColor: document.querySelector('body'),
+}
+    let randomHexColor = null;
+    let interval = null;
 
+refs.btnStart.addEventListener('click', onBtnStart);
+refs.btnStop.addEventListener('click', onBtnStop);
+
+refs.btnStop.setAttribute('disabled', '');
+
+    function onBtnStart() {
+        refs.btnStop.removeAttribute('disabled');
+        refs.btnStart.setAttribute('disabled', '');
+        interval = setInterval(() => changeBodyBgColor(), 1000);
+    }
+
+    function onBtnStop() {
+        refs.btnStart.removeAttribute('disabled');
+        refs.btnStop.setAttribute('disabled', '');
+        clearInterval(interval);
+    }
+    function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+
+    function changeBodyBgColor() {
+        randomHexColor = getRandomHexColor();
+        refs.bodyColor.style.backgroundColor = randomHexColor;
+    }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onSubmitBtn);
+formEl.addEventListener('input', onInput);
+
+let data = {};
+
+function onInput(e) {
+  data[e.target.name] = Number(e.target.value);
+}
+
+function onSubmitBtn(e) {
+  e.preventDefault();
+  promiseLoop();
+  e.currentTarget.reset();
+}
+
+function promiseLoop() {
+  for (let i = 1; i <= data.amount; i += 1) {
+    let delay = (data.delay += data.step) - data.step;
+
+    setTimeout(() => {
+      createPromise(i, delay);
+    }, delay);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    notiFySucces(position, delay);
   } else {
-    // Reject
+    notiFyFailure(position, delay);
   }
+}
+
+function notiFySucces(position, delay) {
+  Notify.success(`Fullfilled promise ${position} in ${delay} ms`);
+}
+
+function notiFyFailure(position, delay) {
+  Notify.failure(`Rejected promise ${position} in ${delay} ms`);
 }
